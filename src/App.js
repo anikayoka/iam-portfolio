b677f7527a155483cb6fff6e0b859cfd0c224550
sunchaser <anika.mclean@gmail.com>
2022-07-07 00:31:43 -0400

    add to  app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Header from "./components/Header";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import About from "./components/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
+    <main> 
+      <About />
+      <Portfolio />
+      <Contact />
+      <Resume />
+    </main>
+    <Footer />
     </div>
   );
 }
