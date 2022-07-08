7146e65d5386ccb4d3e24778ef77d705c39f89d0
sunchaser <anika.mclean@gmail.com>
2022-07-08 01:05:46 -0400

    add about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function About() {
 
   return (
-    <section className="my-5" >
-      {/* <h1 id="about">Who I AM?</h1> */}
-      <img src={process.env.PUBLIC_URL + '/assets/images/aboutpic.png'} className="my-2" style={{ width: "25%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-        Full stack web developer with graphic design experience, leveraging a finance/accounting background. Recent graduate of University of Toronto coding bootcamp - newly developed skills in JavaScript, HTML, CSS, MERN: MongoDB Express.js React Node.js, SQL, APIs, Graphql and more. My aim is to develop as a software engineer and engage my audience for an impactful user experience. Known as a problem solver with a meticulous nature, passionate about web development and all things numbers related. I am excited to power my skills as part of a fast-paced, quality-driven team to build better experiences on the web and to help solidify my talents as a web developer.
-      </p>
-      </div>
-    </section>
+    <div className="about-container">
+      <section className="aboutHeader my-5" >
+        <h1 id="about">Who i AM!</h1>
+        <img src={process.env.PUBLIC_URL + '/assets/images/aboutpic.jpg'} className="aboutImage my-2" style={{ width: "25%" }} alt="Anika McLean" />
+        <div className="my-2">
+          <h2>Welcome!</h2>
+          <p>
+            I am a Full stack web developer with graphic design experience, leveraging a finance/accounting background. A recent graduate of University of Toronto coding bootcamp, with newly developed skills in JavaScript, HTML, CSS, MERN: MongoDB Express.js React Node.js, SQL, APIs, Graphql and more.
+            <br />
+            I aim to evolve as a software engineer and engage my audience for an impactful user experience. Known as a problem solver with a meticulous nature, passionate about web development and all things numbers related. I am excited to power my skills as part of a fast-paced, quality-driven team to build better experiences on the web and to help solidify my talents as a web developer.
+          </p>
+        </div>
+      </section>
+    </div>
   )
 }
 
-export default About
+export default About;
