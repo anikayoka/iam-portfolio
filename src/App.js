798a006db16c80a4628362d3c94f878b90639bc7
sunchaser <anika.mclean@gmail.com>
2022-07-08 02:59:31 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 import Header from "./components/Header";
 import About from "./components/About";
-import Projects from "./components/Projects";
+import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
+  const [categories] = useState([
+    {
+      name: "About"
+    },
+    {
+      name: "Portfolio",
+    },
+    {
+      name: "Contact",
+    },
+    {
+      name: "Resume",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0].name);
+
   return (
-    <div>
-      {/* <Header /> */}
-    <main> 
-      <About />
-      <Projects />
-      {/* <Contact />
-      <Resume /> */}
-    </main>
-    {/* <Footer /> */}
+    <div className='main-container'>
+      <Header
+        categories={categories}
+        currentCategory={currentCategory}
+        setCurrentCategory={setCurrentCategory}>
+      </Header>
+
+      <main className="content-wrapper">
+        {(() => {
+          if (currentCategory === 'About') {
+            return (
+              <div>
+                <About></About>
+              </div>
+            );
+          }
+          else if (currentCategory === 'Portfolio') {
+            return (
+              <div>
+                <Portfolio></Portfolio>
+              </div>
+            );
+          }
+          else if (currentCategory === 'Contact') {
+            return (
+              <div>
+                <Contact></Contact>
+              </div>
+            );
+          } else if (currentCategory === 'Resume') {
+            return (
+              <div>
+                <Resume></Resume>
+              </div>
+
+            );
+          }
+          return <About></About>
+        })()}
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 
 const Footer = () => {
-  return(
-    <footer className="footer">
-      <div className="content has-text-centered">
-        <p>
-          <strong>Portfolio</strong> - Made with ❤️ by {""} <a href="https://github.com/anikayoka">Anika McLean</a>
-        </p>
-      </div>
-    </footer>
-  )
-
+  return (
+    <div className="footer">
+      <div className="footer-list">
+        <li className="mx-3">
+          Made with ❤️ by Anika McLean
+        </li>
+        <li className="mx-3">
+        <a href="https://github.com/anikayoka"></a><span className="bi bi-github"></span>
+        </li>
+        <li className="mx-3">
+        &copy; 2022 Portfolio, Inc.
+        </li>
+       </div> 
+    </div>
+  );
 }
 
 export default Footer;
